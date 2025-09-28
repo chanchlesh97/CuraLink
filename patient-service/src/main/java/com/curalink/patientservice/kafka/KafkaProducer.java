8141f55b52ffd65f40b46a08a69014a18f47dcfb
Chanchlesh <Chanchlesh.dev@gmail.com>
2025-09-28 09:14:54 +0530

    Add basic flow for kafka producer consumer

---
+++ patient-service/src/main/java/com/curalink/patientservice/kafka/KafkaProducer.java
@@ -0,0 +1,35 @@
+package com.curalink.patientservice.kafka;
+
+import com.curalink.patientservice.model.Patient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+import patient.events.PatientEvents;
+
+@Service
+public class KafkaProducer {
+    private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEvent(Patient patient) {
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventType("PATIENT_CREATED")
+                .build();
+
+        try {
+            kafkaTemplate.send("patients", event.toByteArray());
+            System.out.println("Sent event to Kafka: " + event);
+        } catch (Exception e) {
+            log.warn("Failed to send Patient Created event to Kafka: {}", e.getMessage());
+        }
+    }
+}
